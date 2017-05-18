d3e5ed80b457961a1605ca1f07686d687fb262a6
Chidambara Natarajan Gopal <chigopal@oradev.oraclecorp.com>
2017-05-18 14:38:08 -0700

    initial commit

--- patient1.go
+++ patient1.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"encoding/json"
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+    "errors"
+    "fmt"
+    "strconv"
+	  "encoding/json"
+    "github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 var logger = shim.NewLogger("mylogger")
@@ -14,174 +15,182 @@ type SampleChaincode struct {
 
 //custom data models
 type PatientInfo struct {
-	PatientId string `json:"patientid"`
-	Zipcd  string `json:"zipcd"`
-	State       string `json:"state"`
-	Birthdate     string `json:"birthdate"`
-	LastModifiedDate    string `json:"lastmodifieddate"`
-	CreateDate   string `json:"createdate"`
+    PatientId string `json:"patientid"`
+    Zipcd  string `json:"zipcd"`
+    State       string `json:"state"`
+    Birthdate     string `json:"birthdate"`
+    LastModifiedDate    string `json:"lastmodifieddate"`
+    CreateDate   string `json:"createdate"`
 }
 
 type InsuranceInfo struct {
-	Provider      int `json:"provider"`
+    Provider      int `json:"provider"`
 }
 
 type ClaimInfo struct {
-	ClaimInfo              string        `json:"id"`
-	PatientId             string        `json:"propertyId"`
-	DateOfVisit           string        `json:"dateOfVisit"`
-	NPI                   string        `json:"npi"`
-	CPT                string        `json:"cpt"`
-	ICD10                 string        `json:"icd10"`
-	NDC                   string        `json:"ndc"`
-	PatientInfo           PatientInfo    `json:"personalInfo"`
-	InsuranceInfo          InsuranceInfo `json:"insuranceInfo"`
-	Cost                 string        `json:"cost"`
-	ProcedureStatus      string        `json:"procedureStatus"`
+    ClaimInfo              string        `json:"id"`
+    PatientId             string        `json:"propertyId"`
+    DateOfVisit           string        `json:"dateOfVisit"`
+    NPI                   string        `json:"npi"`
+    CPT                string        `json:"cpt"`
+    ICD10                 string        `json:"icd10"`
+    NDC                   string        `json:"ndc"`
+    PatientInfo           PatientInfo    `json:"personalInfo"`
+    InsuranceInfo          InsuranceInfo `json:"insuranceInfo"`
+    Cost                 string        `json:"cost"`
+    ProcedureStatus      string        `json:"procedureStatus"`
+}
+var accountIndexStr = "_accountindex"	  // Define an index varibale to track all the accounts stored in the world state
+
+func GetPatientInfo(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    logger.Debug("Entering GetPatientInfo")
+
+    if len(args) < 1 {
+        logger.Error("Invalid number of arguments")
+        return nil, errors.New("Missing patient ID")
+    }
+
+    var patientID = args[0]
+    bytes, err := stub.GetState(patientID)
+    if err != nil {
+        logger.Error("Could not fetch patient info with id "+patientID+" from ledger", err)
+        return nil, err
+    }
+    return bytes, nil
 }
 
-func GetLoanApplication(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	logger.Debug("Entering GetLoanApplication")
+func CreatePatientInfo(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    logger.Debug("Entering CreatePatientInfo")
 
-	if len(args) < 1 {
-		logger.Error("Invalid number of arguments")
-		return nil, errors.New("Missing loan application ID")
-	}
+    if len(args) < 2 {
+        logger.Error("Invalid number of args")
+        return nil, errors.New("Expected at least two arguments for patient info creation.")
+    }
 
-	var loanApplicationId = args[0]
-	bytes, err := stub.GetState(loanApplicationId)
-	if err != nil {
-		logger.Error("Could not fetch loan application with id "+loanApplicationId+" from ledger", err)
-		return nil, err
-	}
-	return bytes, nil
-}
+    var patientID = args[0]
+    var patientInfoInput = args[1]
 
-func CreateLoanApplication(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	logger.Debug("Entering CreateLoanApplication")
+    err := stub.PutState(patientID, []byte(patientInfoInput))
+    if err != nil {
+        logger.Error("Could not save patient info to ledger", err)
+        return nil, err
+    }
 
-	if len(args) < 2 {
-		logger.Error("Invalid number of args")
-		return nil, errors.New("Expected atleast two arguments for loan application creation")
-	}
-
-	var loanApplicationId = args[0]
-	var loanApplicationInput = args[1]
-
-	err := stub.PutState(loanApplicationId, []byte(loanApplicationInput))
-	if err != nil {
-		logger.Error("Could not save loan application to ledger", err)
-		return nil, err
-	}
-
-	var customEvent = "{eventType: 'loanApplicationCreation', description:" + loanApplicationId + "' Successfully created'}"
-	err = stub.SetEvent("evtSender", []byte(customEvent))
-	if err != nil {
-		return nil, err
-	}
-	logger.Info("Successfully saved loan application")
-	return nil, nil
+    // var customEvent = "{eventType: 'patientInfoCreation', description: " + patientID + "' Successfully created'}"
+    // err = stub.SetEvent("evtSender", []byte(customEvent))
+    // if err != nil {
+    //     return nil, err
+    // }
+    logger.Info("Successfully saved patient info.")
+    return nil, nil
 
 }
 
-/**
-Updates the status of the loan application
-**/
-func UpdateLoanApplication(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	logger.Debug("Entering UpdateLoanApplication")
+func CreateClaimInfo(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    logger.Debug("Entering CreateClaimInfo")
 
-	if len(args) < 2 {
-		logger.Error("Invalid number of args")
-		return nil, errors.New("Expected atleast two arguments for loan application update")
-	}
+    if len(args) < 2 {
+        logger.Error("Invalid number of args")
+        return nil, errors.New("Expected at least two arguments for claim info creation.")
+    }
 
-	var loanApplicationId = args[0]
-	var status = args[1]
+    var claimId = args[0]
+    var claimInfoInput = args[1]
 
-	laBytes, err := stub.GetState(loanApplicationId)
-	if err != nil {
-		logger.Error("Could not fetch loan application from ledger", err)
-		return nil, err
-	}
-	var loanApplication LoanApplication
-	err = json.Unmarshal(laBytes, &loanApplication)
-	loanApplication.Status = status
+    err := stub.PutState(claimId, []byte(claimInfoInput))
+    if err != nil {
+        logger.Error("Could not save claim info to ledger", err)
+        return nil, err
+    }
 
-	laBytes, err = json.Marshal(&loanApplication)
-	if err != nil {
-		logger.Error("Could not marshal loan application post update", err)
-		return nil, err
-	}
-
-	err = stub.PutState(loanApplicationId, laBytes)
-	if err != nil {
-		logger.Error("Could not save loan application post update", err)
-		return nil, err
-	}
-
-	var customEvent = "{eventType: 'loanApplicationUpdate', description:" + loanApplicationId + "' Successfully updated status'}"
-	err = stub.SetEvent("evtSender", []byte(customEvent))
-	if err != nil {
-		return nil, err
-	}
-	logger.Info("Successfully updated loan application")
-	return nil, nil
+    // var customEvent = "{eventType: 'claimInfoCreation', description: " + claimId + "' Successfully created'}"
+    // err = stub.SetEvent("evtSender", []byte(customEvent))
+    // if err != nil {
+    //     return nil, err
+    // }
+    logger.Info("Successfully saved patient info.")
+    return nil, nil
 
 }
 
 func (t *SampleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	return nil, nil
+    var Aval int
+    var err error
+
+    if len(args) != 1 {
+      return nil, errors.New("Incorrect number of arguments. Expecting a single integer")
+    }
+
+    // Initialize the chaincode
+    Aval, err = strconv.Atoi(args[0])
+    if err != nil {
+      return nil, errors.New("Expecting integer value for testing the blockchain network")
+    }
+
+    // Write the state to the ledger, test the network
+    err = stub.PutState("test_key", []byte(strconv.Itoa(Aval)))
+    if err != nil {
+      return nil, err
+    }
+
+    var empty []string
+    jsonAsBytes, _ := json.Marshal(empty)								//marshal an emtpy array of strings to clear the account index
+    err = stub.PutState(accountIndexStr, jsonAsBytes)
+    if err != nil {
+      return nil, err
+    }
+
+    return nil, nil
 }
+
+
 
 func (t *SampleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function == "GetLoanApplication" {
-		return GetLoanApplication(stub, args)
-	}
-	return nil, nil
+    if function == "GetPatientInfo" {
+        return GetPatientInfo(stub, args)
+    }
+    return nil, nil
 }
 
-func GetCertAttribute(stub shim.ChaincodeStubInterface, attributeName string) (string, error) {
-	logger.Debug("Entering GetCertAttribute")
-	attr, err := stub.ReadCertAttribute(attributeName)
-	if err != nil {
-		return "", errors.New("Couldn't get attribute " + attributeName + ". Error: " + err.Error())
-	}
-	attrString := string(attr)
-	return attrString, nil
-}
+// func GetCertAttribute(stub shim.ChaincodeStubInterface, attributeName string) (string, error) {
+//     logger.Debug("Entering GetCertAttribute")
+//     attr, err := stub.ReadCertAttribute(attributeName)
+//     if err != nil {
+//         return "", errors.New("Couldn't get attribute " + attributeName + ". Error: " + err.Error())
+//     }
+//     attrString := string(attr)
+//     return attrString, nil
+// }
 
 func (t *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function == "CreateLoanApplication" {
-		username, _ := GetCertAttribute(stub, "username")
-		role, _ := GetCertAttribute(stub, "role")
-		if role == "Bank_Home_Loan_Admin" {
-			return CreateLoanApplication(stub, args)
-		} else {
-			return nil, errors.New(username + " with role " + role + " does not have access to create a loan application")
-		}
+    if function == "CreatePatientInfo" {
+        return CreatePatientInfo(stub, args)
+    }
 
-	}
-	return nil, nil
+    if function == "CreateClaimInfo" {
+        return CreateClaimInfo(stub, args)
+    }
+    return nil, nil
 }
 
 type customEvent struct {
-	Type       string `json:"type"`
-	Decription string `json:"description"`
+    Type       string `json:"type"`
+    Decription string `json:"description"`
 }
 
 func main() {
 
-	lld, _ := shim.LogLevel("DEBUG")
-	fmt.Println(lld)
+    lld, _ := shim.LogLevel("DEBUG")
+    fmt.Println(lld)
 
-	logger.SetLevel(lld)
-	fmt.Println(logger.IsEnabledFor(lld))
+    logger.SetLevel(lld)
+    fmt.Println(logger.IsEnabledFor(lld))
 
-	err := shim.Start(new(SampleChaincode))
-	if err != nil {
-		logger.Error("Could not start SampleChaincode")
-	} else {
-		logger.Info("SampleChaincode successfully started")
-	}
+    err := shim.Start(new(SampleChaincode))
+    if err != nil {
+        logger.Error("Could not start SampleChaincode")
+    } else {
+        logger.Info("SampleChaincode successfully started")
+    }
 
 }
